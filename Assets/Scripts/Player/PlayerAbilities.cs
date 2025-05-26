26494a3ac05797d8b7901c2245690439225a5c80
Casper Mårtensson <casper.martensson@elev.ga.lbs.se>
2025-05-26 14:48:58 +0200

    Jump & Dash

--- Assets/Scripts/Player/PlayerAbilities.cs
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -1,18 +1,56 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerAbilities : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI tempAbilityDisplay;
+
     private Abilities currentAbility;
+
+    private PlayerMovement playerMovement;
 
     private void Start()
     {
+        playerMovement = FindFirstObjectByType<PlayerMovement>();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseAbility();
+        }
     }
 
     public void ChangeAbility()
     {
         currentAbility++;
-        Debug.Log(currentAbility);
+
+        if (currentAbility == Abilities.Total)
+        {
+            currentAbility = 0;
+        }
+
+        tempAbilityDisplay.text = "Current Ability: " + currentAbility;
+    }
+
+    public void UseAbility()
+    {
+        switch (currentAbility)
+        {
+            case Abilities.Jump:
+                playerMovement.Jump();
+                break;
+            case Abilities.Dash:
+                playerMovement.Dash();
+                break;
+            case Abilities.GroundPound:
+                break;
+            case Abilities.Sword:
+                break;
+            case Abilities.WallBreak:
+                break;
+        }
     }
 
     private enum Abilities
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayerMask;
 
     private bool moveBlock;
+    private float currentMovementSpeed;
 
     private Rigidbody2D playerRigidbody;
 
@@ -13,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+
+        currentMovementSpeed = movementSpeed;
     }
 
     private void Update()
@@ -22,15 +33,70 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
-        float inputFloat = Input.GetAxisRaw("Horizontal");
+        float inputFloat;
+
+        if (currentMovementSpeed == dashSpeed)
+        {
+            inputFloat = transform.localScale.x;
+        }
+        else
+        {
+            inputFloat = Input.GetAxisRaw("Horizontal");
+        }
 
         if (Mathf.Abs(inputFloat) > Mathf.Epsilon)
         {
-            playerRigidbody.linearVelocity = new Vector2(inputFloat * movementSpeed, playerRigidbody.linearVelocity.y);
+            playerRigidbody.linearVelocity = new Vector2(inputFloat * currentMovementSpeed, playerRigidbody.linearVelocity.y);
         }
         else
         {
             playerRigidbody.linearVelocity = new Vector2(0, playerRigidbody.linearVelocity.y);
         }
+
+        if (Mathf.Abs(playerRigidbody.linearVelocity.x) > Mathf.Epsilon)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.linearVelocity.x), transform.localScale.y);
+        }
+    }
+
+    public void Jump()
+    {
+        if (!moveBlock && GroundCheck())
+        {
+            playerRigidbody.AddForce(new Vector2(0, jumpForce * 1000));
+        }
+    }
+
+    bool GroundCheck()
+    {
+        if (Physics2D.Raycast(transform.position, -transform.up, groundCheckDistance, groundLayerMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Dash()
+    {
+        StartCoroutine(DashRoutine());
+    }
+
+    IEnumerator DashRoutine()
+    {
+        currentMovementSpeed = dashSpeed;
+
+        yield return new WaitForSeconds(dashTime);
+
+        currentMovementSpeed = movementSpeed;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Vector3 groundPos = new Vector3(transform.position.x, transform.position.y - groundCheckDistance, transform.position.z);
+        Gizmos.DrawLine(transform.position, groundPos);
     }
 }
